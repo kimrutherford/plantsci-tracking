e6ba7039b7799d37e09ca3d8fd2ac956ecb90d80
kmr44 <kmr44@721a6f05-5756-4a90-b3fc-8e2b6c5fda0e>
2009-07-16 15:42:43 +0000

    Added code to convert GFF3 files to GFF2.
    git-svn-id: file:///data/subversion/smallrna/pipeline/trunk@679 721a6f05-5756-4a90-b3fc-8e2b6c5fda0e

---
+++ lib/SmallRNA/Process/GFF3ToGFF2Process.pm
@@ -0,0 +1,96 @@
+package SmallRNA::Process::GFF3ToGFF2Process;
+
+=head1 NAME
+
+SmallRNA::Process::GFF3ToGFF2Process - Convert a GFF3 file to a GFF2 file
+
+=head1 SYNOPSIS
+
+=head1 AUTHOR
+
+Kim Rutherford C<< <kmr44@cam.ac.uk> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<kmr44@cam.ac.uk>.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc SmallRNA::Process::GFF3ToGFF2Process
+
+You can also look for information at:
+
+=over 4
+
+=back
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Kim Rutherford, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 FUNCTIONS
+
+=cut
+
+use strict;
+use Carp;
+use Bio::SeqIO;
+use Params::Validate qw(:all);
+use warnings;
+
+=head2
+
+ Usage   : SmallRNA::Process::GFF3ToGFF2Process::run(input_file_name =>
+                                                       $in_file_name,
+                                                     output_file_name =>
+                                                       $out_file_name,
+                                                     sample_name =>
+                                                       $sample_name);
+ Function: Convert a GFF3 file to a GFF2 file
+ Args    : input_file_name - the input file name
+           output_file_name - the name of the file to write the GFF2 to
+           sample_name - the name of the sample that generated this file
+ Returns : nothing - either succeeds or calls croak()
+
+=cut
+sub run
+{
+  my %params = validate(@_, { input_file_name => 1, output_file_name => 1,
+                              sample_name => 1 });
+
+  if (!-e $params{input_file_name}) {
+    croak "can't find input file: $params{input_file_name}";
+  }
+
+  open my $in_file, '<', $params{input_file_name}
+    or croak "can't open $params{input_file_name} for reading: $!";
+
+  open my $out_file, '>', $params{output_file_name}
+    or croak "can't open $params{output_file_name} for reading: $!";
+
+  while (defined (my $line = <$in_file>)) {
+    next if $line =~ /^\s*#/;
+
+    my @bits = split (/\t/, $line);
+
+    if (!($bits[0] =~ s/^Chr//)) {
+      croak "pattern match failed\n";
+    }
+
+    $bits[8] =~ s/.*Note=(\w+).*/$params{sample_name} $1/;
+
+    my $new_line = join "\t", @bits;
+
+    print $out_file $new_line;
+  }
+
+  close $in_file or croak "can't close $params{input_file_name}: $!\n";
+  close $out_file or croak "can't close $params{output_file_name}: $!\n";
+}
+
+1;
---
+++ lib/SmallRNA/Runable/GFF3ToGFF2Runable.pm
@@ -0,0 +1,112 @@
+package SmallRNA::Runable::GFF3ToGFF2Runable;
+
+=head1 NAME
+
+SmallRNA::Runable::GFF3ToGFF2Runable - Create a GFF2 file from a GFF3 file
+
+=head1 SYNOPSIS
+
+=head1 AUTHOR
+
+Kim Rutherford C<< <kmr44@cam.ac.uk> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<kmr44@cam.ac.uk>.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc SmallRNA::Runable::GFF3ToGFF2Runable
+
+=over 4
+
+=back
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Kim Rutherford, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 FUNCTIONS
+
+=cut
+
+use strict;
+use warnings;
+use Carp;
+
+use Moose;
+
+use SmallRNA::Index::Manager;
+
+extends 'SmallRNA::Runable::SmallRNARunable';
+
+=head2
+
+ Function: Create a GFF2 file from a GFF3 file
+ Returns : nothing - either succeeds or calls die()
+
+=cut
+sub run
+{
+  my $self = shift;
+  my $schema = $self->schema();
+
+  my $code = sub {
+    my $pipeprocess = $self->pipeprocess();
+
+    my @input_pipedatas = $pipeprocess->input_pipedatas();
+    if (@input_pipedatas > 1) {
+      croak ("pipeprocess ", $pipeprocess->pipeprocess_id(),
+             " has more than one input pipedata\n");
+    }
+    my $input_pipedata = $input_pipedatas[0];
+
+    my @samples = $input_pipedata->samples();
+
+    if (@samples > 1) {
+      croak ("pipedata for pipeprocess ", $pipeprocess->pipeprocess_id(),
+             " has more than one sample\n")
+    }
+
+    my $sample = $samples[0];
+
+    my $sample_name = $sample->name();
+
+    my $input_format_type = $input_pipedata->format_type()->name();
+    my $input_content_type = $input_pipedata->content_type()->name();
+
+    if ($input_format_type eq 'gff3') {
+      croak('must have GFF3 as input');
+    }
+
+    my $output_type = 'gff2';
+    my $data_dir = $self->config()->data_directory();
+
+    my $input_file_name = $data_dir . '/' . $input_pipedata->file_name();
+
+    my $output_file_name = $input_file_name;
+
+    if ($output_file_name =~ s/\.$input_format_type$/.$output_type/) {
+      SmallRNA::Process::GFF2ToGFF2Process::run(input_file_name =>
+                                                  "$data_dir/" . $input_file_name,
+                                                output_file_name =>
+                                                  "$data_dir/" . $output_file_name,
+                                                sample_name => $sample_name);
+
+      $self->store_pipedata(generating_pipeprocess => $self->pipeprocess(),
+                            file_name => $gff_file_name,
+                            format_type_name => $output_type,
+                            content_type_name => $input_content_type);
+    } else {
+      croak("pattern match failed on: ", $output_file_name);
+    }
+  };
+  $self->schema->txn_do($code);
+}
+
+1;
