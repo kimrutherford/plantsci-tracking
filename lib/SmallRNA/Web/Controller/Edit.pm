1ee0a37a0f2156a56ad7b2385825177e5654178d
kmr44 <kmr44@721a6f05-5756-4a90-b3fc-8e2b6c5fda0e>
2009-07-28 09:51:54 +0000

    More arranging.
    git-svn-id: file:///data/subversion/smallrna/pipeline/trunk@696 721a6f05-5756-4a90-b3fc-8e2b6c5fda0e

--- lib/SmallRNA/Web/Controller/Edit.pm
+++ lib/SmallRNA/Web/Controller/Edit.pm
@@ -49,7 +49,6 @@ sub _get_field_values
   my $c = shift;
   my $table_name = shift;
   my $class_name = shift;
-  my $field_name = shift;
   my $select_value = shift;
   my $field_info = shift;
 
@@ -100,16 +99,110 @@ sub _get_field_values
 # the names of buttons in the form so we can skip them later
 my @INPUT_BUTTON_NAMES = qw(submit cancel);
 
+sub _init_form_field
+{
+  my $c = shift;
+  my $field_info = shift;
+  my $object = shift;
+
+  my $type = $object->table();
+
+  my $field_label = $field_info->{field_label};
+
+  my $display_field_label = $field_label;
+  $display_field_label =~ s/_/ /g;
+
+  next unless $field_info->{editable};
+
+  my $field_db_column = $field_label;
+
+  if (defined $field_info->{field_conf}) {
+    $field_db_column = $field_info->{field_conf};
+  }
+
+  my $elem = {
+    name => $field_db_column, label => $display_field_label
+  };
+
+  my $class_name = SmallRNA::DB::class_name_of_table($type);
+  my $db_source = $c->schema()->source($class_name);
+
+  my $info_ref;
+
+  if (!$field_info->{is_collection}) {
+    $info_ref = $class_name->relationship_info($field_db_column);
+  }
+
+  if (defined $info_ref) {
+    my %info = %{$info_ref};
+    my $referenced_class_name = $info{class};
+
+    my $referenced_table = SmallRNA::DB::table_name_of_class($referenced_class_name);
+
+    if (!defined $field_label) {
+      die "no display_key_fields configuration for $referenced_table\n";
+    }
+
+    $elem->{type} = 'Select';
+    my $table_id_column = $referenced_table . '_id';
+
+    my $current_value = undef;
+    if (defined $object && defined $object->$field_db_column()) {
+      $current_value = $object->$field_db_column()->$table_id_column();
+    } else {
+      $current_value = $c->req->param("$referenced_table.id");
+    }
+
+    $elem->{options} = [_get_field_values($c, $referenced_table,
+                                          $referenced_class_name,
+                                          $current_value, $field_info)];
+
+    my $field_is_nullable = $db_source->column_info($field_db_column)->{is_nullable};
+
+    if ($field_is_nullable) {
+      # add a blank to the select list if this field can be null
+      unshift @{$elem->{options}}, [0, ''];
+    }
+  } else {
+    if ($field_info->{is_collection}) {
+
+    } else {
+      $elem->{type} = 'Text';
+      if (!$db_source->column_info($field_db_column)->{is_nullable}) {
+        $elem->{constraints} = [ { type => 'Length',  min => 1 },
+          'Required' ];
+      }
+      if (defined $object) {
+        $elem->{value} = $object->$field_db_column();
+      } else {
+        # try to set the default value (if configured)
+        my $default_value_code = $field_info->{default_value};
+
+        if (defined $default_value_code) {
+          $elem->{value} = eval "$default_value_code";
+          if ($@) {
+            warn "error evaluating default_value configuration for "
+              . "'$field_label': $@";
+          }
+        }
+      }
+    }
+  }
+
+  return $elem;
+}
+
 # Initialise the form using the list of field_infos in the config file.
 # Attributes will be rendered as text areas, references as pop ups.
 sub _initialise_form
 {
   my $c = shift;
-  my $type = shift;
   my $object = shift;
   my $form = shift;
 
   my @elements = ();
+
+  my $type = $object->table();
 
   my $class_info_ref = $c->config()->{class_info}->{$type};
   if (!defined $class_info_ref) {
@@ -124,85 +217,12 @@ sub _initialise_form
   my @field_infos = @{$c->config()->{class_info}->{$type}->{field_info_list}};
 
   for my $field_info (@field_infos) {
-    my $field_label = $field_info->{field_label};
-
-    my $display_field_label = $field_label;
-    $display_field_label =~ s/_/ /g;
-
-    next unless $field_info->{editable};
-
-    my $field_db_column = $field_label;
-
-    if (defined $field_info->{field_conf}) {
-      $field_db_column = $field_info->{field_conf};
-    }
-
-    my $elem = {
-      name => $field_db_column, label => $display_field_label
-    };
-
-    if ($field_info->{is_collection}) {
-
-    } else {
-      my $class_name = SmallRNA::DB::class_name_of_table($type);
-      my $info_ref = $class_name->relationship_info($field_db_column);
-      my $db_source = $c->schema()->source($class_name);
-      my $field_is_nullable = $db_source->column_info($field_db_column)->{is_nullable};
-
-      if (defined $info_ref) {
-        my %info = %{$info_ref};
-        my $referenced_class_name = $info{class};
-
-        my $referenced_table = SmallRNA::DB::table_name_of_class($referenced_class_name);
-
-        if (!defined $display_field) {
-          die "no display_key_fields configuration for $referenced_table\n";
-        }
-
-        $elem->{type} = 'Select';
-        my $table_id_column = $referenced_table . '_id';
-
-        my $current_value = undef;
-        if (defined $object && defined $object->$field_db_column()) {
-          $current_value = $object->$field_db_column()->$table_id_column();
-        } else {
-          $current_value = $c->req->param("$referenced_table.id");
-        }
-
-        $elem->{options} = [_get_field_values($c, $referenced_table,
-                                              $referenced_class_name, $display_field,
-                                              $current_value, $field_info)];
-
-        if ($field_is_nullable) {
-          # add a blank to the select list if this field can be null
-          unshift @{$elem->{options}}, [0, ''];
-        }
-      } else {
-        $elem->{type} = 'Text';
-        if (!$field_is_nullable) {
-          $elem->{constraints} = [ { type => 'Length',  min => 1 },
-                                   'Required' ];
-        }
-        if (defined $object) {
-          $elem->{value} = $object->$field_db_column();
-        } else {
-          # try to set the default value (if configured)
-          my $default_value_code = $field_info->{default_value};
-
-          if (defined $default_value_code) {
-            $elem->{value} = eval "$default_value_code";
-            if ($@) {
-              warn "error evaluating default_value configuration for "
-                . "'$field_label': $@";
-            }
-          }
-        }
-      }
-    }
-
-    push @elements, $elem;
+    push @elements, _init_form_field($c, $field_info, $object);
   }
 
+    use Data::Dumper;
+
+    warn "XXXXXXXXXXXXFOO:", Data::Dumper->Dump([@elements]);
   $form->auto_fieldset(1);
   $form->elements([
                     @elements,
@@ -210,6 +230,7 @@ sub _initialise_form
                       name => $_, type => 'Submit', value => ucfirst $_
                     } } @INPUT_BUTTON_NAMES,
                   ]);
+
 }
 
 sub _create_object {
@@ -301,7 +322,7 @@ sub object : Regex('(new|edit)/object/([^/]+)(?:/([^/]+))?') {
 
   my $form = $self->form;
 
-  _initialise_form($c, $type, $object, $form);
+  _initialise_form($c, $object, $form);
 
   $form->process;
 
