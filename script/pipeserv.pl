c842e966fd4851aad35888672c67b3d32707dfe9
kmr44 <kmr44@721a6f05-5756-4a90-b3fc-8e2b6c5fda0e>
2009-08-04 11:21:14 +0000

    Fixed pipeserv so that when it tries to find jobs to run it queries only for those jobs that have status of "not_started". Changed what happens in one transaction - hopefully to improve transactional safety.
    git-svn-id: file:///data/subversion/smallrna/pipeline/trunk@765 721a6f05-5756-4a90-b3fc-8e2b6c5fda0e

--- script/pipeserv.pl
+++ script/pipeserv.pl
@@ -22,66 +22,68 @@ my $proc_manager = SmallRNA::ProcessManager->new(schema => $schema);
 
 $proc_manager->create_new_pipeprocesses();
 
-my $conf_rs = $schema->resultset('Pipeprocess');
 my $queued_status = $schema->find_with_type('Cvterm', name => 'queued');
+
+my $not_started_status = $schema->find_with_type('Cvterm', name => 'not_started');
+my $conf_rs = 
+  $schema->resultset('Pipeprocess')->search({ status => $not_started_status->cvterm_id() });
 my $test_mode = $ENV{SMALLRNA_PIPELINE_TEST} || 0;
 
 while (my $pipeprocess = $conf_rs->next()) {
   my $code = sub {
-    if ($pipeprocess->status()->name() eq 'not_started') {
-      my $pipeprocess_id = $pipeprocess->pipeprocess_id();
-      my $pipework_path = "$Bin/pipework.pl";
+    my $pipeprocess_id = $pipeprocess->pipeprocess_id();
+    my $pipework_path = "$Bin/pipework.pl";
 
-      my $command =
-        "qsub -v PIPEPROCESS_ID=$pipeprocess_id,SMALLRNA_PIPELINE_TEST=$test_mode $pipework_path";
+    $pipeprocess->time_queued(DateTime->now());
+    $pipeprocess->status($queued_status);
 
-      open my $qsub_handle, '-|', $command
-        or die "couldn't open pipe from qsub: $!\n";
+    my $command =
+      "qsub -v PIPEPROCESS_ID=$pipeprocess_id,SMALLRNA_PIPELINE_TEST=$test_mode $pipework_path";
 
-      my $qsub_jobid = <$qsub_handle>;
+    open my $qsub_handle, '-|', $command
+      or die "couldn't open pipe from qsub: $!\n";
 
-      chomp $qsub_jobid;
+    my $qsub_jobid = <$qsub_handle>;
 
-      if (!defined $qsub_handle) {
-        die "failed to read the job id from qsub command: $!\n";
-      }
+    chomp $qsub_jobid;
 
-      chomp $qsub_handle;
-
-      # finish reading everything from the pipe so that qsub doesn't get a SIGPIPE
-      1 while (<$qsub_handle>);
-
-      warn "started job with pipeprocess_id: $pipeprocess_id and job id: $qsub_jobid\n";
-
-      close $qsub_handle or die "couldn't close pipe from qsub: $!\n";
-
-      $pipeprocess->job_identifier($qsub_jobid);
-      $pipeprocess->time_queued(DateTime->now());
-      $pipeprocess->status($queued_status);
-      $pipeprocess->update();
-
-      if (defined $ENV{'PIPESERV_MAX_JOBS'} && $ENV{'PIPESERV_MAX_JOBS'} > 0) {
-        for (1..1000) {  # don't do it forever, in case there's a problem
-          my $count = 0;
-
-          open PIPE, "qstat|" or die;
-
-          while (defined (my $line = <PIPE>)) {
-            $count++ if $line =~ / R batch/;
-          }
-
-          if ($count >= $ENV{'PIPESERV_MAX_JOBS'}) {
-            warn "$count - sleeping\n";
-            sleep (20);
-          } else {
-            last;
-          }
-        }
-      }
-
-      sleep (1);
+    if (!defined $qsub_handle) {
+      die "failed to read the job id from qsub command: $!\n";
     }
+
+    chomp $qsub_handle;
+
+    # finish reading everything from the pipe so that qsub doesn't get a SIGPIPE
+    1 while (<$qsub_handle>);
+
+    warn "started job with pipeprocess_id: $pipeprocess_id and job id: $qsub_jobid\n";
+
+    close $qsub_handle or die "couldn't close pipe from qsub: $!\n";
+
+    $pipeprocess->job_identifier($qsub_jobid);
+    $pipeprocess->update();
+
+    sleep (1);
   };
 
   $schema->txn_do($code);
+
+  if (defined $ENV{'PIPESERV_MAX_JOBS'} && $ENV{'PIPESERV_MAX_JOBS'} > 0) {
+    for (1..1000) {   # don't do it forever, in case there's a problem
+      my $count = 0;
+
+      open PIPE, "qstat|" or die;
+
+      while (defined (my $line = <PIPE>)) {
+        $count++ if $line =~ / [QR] batch/;
+      }
+
+      if ($count >= $ENV{'PIPESERV_MAX_JOBS'}) {
+        warn "$count - sleeping\n";
+        sleep (20);
+      } else {
+        last;
+      }
+    }
+  }
 }
