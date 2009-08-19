02815e86caab0c0a04e851056f66ec3fa0f044ca
kmr44 <kmr44@721a6f05-5756-4a90-b3fc-8e2b6c5fda0e>
2009-08-19 13:02:21 +0000

    Fixed custom/create_seq_sample action.
    git-svn-id: file:///data/subversion/smallrna/pipeline/trunk@819 721a6f05-5756-4a90-b3fc-8e2b6c5fda0e

--- lib/SmallRNA/Web/Controller/Custom.pm
+++ lib/SmallRNA/Web/Controller/Custom.pm
@@ -41,13 +41,29 @@ use Carp;
 
 use base 'Catalyst::Controller::HTML::FormFu';
 
-sub create_seq_run : Local {
+sub create_seq_sample : Local {
   my ($self, $c) = @_;
 
-  my $sequencing_sample_id = 1;
+  my $sample_id = $c->req->param("sample.id");
+  my $schema = $c->schema();
+
+  my $sample = $schema->find_with_type('Sample', sample_id => $sample_id);
+
+  my $seq_sample = $schema->create_with_type('SequencingSample',
+                                             { name => 'SEQSAMP-' . $sample->name() });
+
+  my $description = 'Dummy coded sample for ', $sample->name(), ' - no barcode';
+  my $initial_run_cvterm = $schema->find_with_type('Cvterm',
+                                                   { name => 'initial run' });
+  my $coded_sample = $schema->create_with_type('CodedSample',
+                                               { description => $description,
+                                                 coded_sample_type => $initial_run_cvterm,
+                                                 sample => $sample,
+                                                 sequencing_sample => $seq_sample });
+
   $c->res->redirect($c->uri_for("/new/object/sequencingrun",
-                                { 
-                                  'sequencing_sample.id' => $sequencing_sample_id
+                                {
+                                  'sequencing_sample.id' => $seq_sample->sequencing_sample_id()
                                 }));
   $c->detach();
 }
