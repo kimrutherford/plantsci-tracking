cc8e874094f17e411af6b047654c77bda6e302bd
kmr44 <kmr44@721a6f05-5756-4a90-b3fc-8e2b6c5fda0e>
2009-08-20 09:56:08 +0000

    Added missing file.
    git-svn-id: file:///data/subversion/smallrna/pipeline/trunk@824 721a6f05-5756-4a90-b3fc-8e2b6c5fda0e

---
+++ lib/SmallRNA/DB/SampleDbxref.pm
@@ -0,0 +1,27 @@
+package SmallRNA::DB::SampleDbxref;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class';
+
+__PACKAGE__->load_components("Core");
+__PACKAGE__->table("sample_dbxref");
+__PACKAGE__->add_columns(
+  "sample_dbxref_id",
+  { data_type => "integer", default_value => undef, is_nullable => 0, size => 4 },
+  "sample_id",
+  { data_type => "integer", default_value => undef, is_nullable => 0, size => 4 },
+  "dbxref_id",
+  { data_type => "integer", default_value => undef, is_nullable => 0, size => 4 },
+);
+__PACKAGE__->belongs_to("dbxref", "SmallRNA::DB::Dbxref", { dbxref_id => "dbxref_id" });
+__PACKAGE__->belongs_to("sample", "SmallRNA::DB::Sample", { sample_id => "sample_id" });
+
+
+# Created by DBIx::Class::Schema::Loader v0.04005
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:rk6ltHcA6pcEUwUr6LS3Fw
+
+
+# You can replace this text with custom content, and it will be preserved on regeneration
+1;
