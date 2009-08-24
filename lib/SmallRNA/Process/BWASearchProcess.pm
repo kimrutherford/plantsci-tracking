5a1eab4f7275b962391917cb6daf3d0ab9bcd27b
kmr44 <kmr44@721a6f05-5756-4a90-b3fc-8e2b6c5fda0e>
2009-08-24 16:52:24 +0000

    Fixed error reporting when bwa or samtools fails.
    git-svn-id: file:///data/subversion/smallrna/pipeline/trunk@848 721a6f05-5756-4a90-b3fc-8e2b6c5fda0e

--- lib/SmallRNA/Process/BWASearchProcess.pm
+++ lib/SmallRNA/Process/BWASearchProcess.pm
@@ -55,18 +55,18 @@ sub _do_system
     warn "system ($system_args) failed: $?\n";
 
     if ($? == -1) {
-      print "failed to execute: $!\n";
+      print STDERR "failed to execute: $!\n";
     }
     elsif ($? & 127) {
-      printf "child died with signal %d, %s coredump\n",
+      printf STDERR "child died with signal %d, %s coredump\n",
         ($? & 127),  ($? & 128) ? 'with' : 'without';
     }
     else {
-      printf "child exited with value %d\n", $? >> 8;
+      printf STDERR "child exited with value %d\n", $? >> 8;
     } 
-  }
 
-  die "command failed\n";
+    die "command failed - exiting\n"; 
+  }
 }
 
 =head2
