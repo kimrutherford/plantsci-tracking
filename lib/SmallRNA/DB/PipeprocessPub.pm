c91eabf125ea37a9b69a33088e10bd605de23b62
kmr44 <kmr44@721a6f05-5756-4a90-b3fc-8e2b6c5fda0e>
2009-08-27 09:58:39 +0000

    Fixed PipeprocessPub to match ad95ea8f2c99bcf7c05d03d87a0e13449b48b86e.
    git-svn-id: file:///data/subversion/smallrna/pipeline/trunk@870 721a6f05-5756-4a90-b3fc-8e2b6c5fda0e

--- lib/SmallRNA/DB/PipeprocessPub.pm
+++ lib/SmallRNA/DB/PipeprocessPub.pm
@@ -9,12 +9,19 @@ __PACKAGE__->load_components("Core");
 __PACKAGE__->table("pipeprocess_pub");
 __PACKAGE__->add_columns(
   "pipeprocess_pub_id",
-  { data_type => "integer", default_value => undef, is_nullable => 0, size => 4 },
+  {
+    data_type => "integer",
+    default_value => "nextval('pipeprocess_pub_pipeprocess_pub_id_seq'::regclass)",
+    is_nullable => 0,
+    size => 4,
+  },
   "pipeprocess_id",
   { data_type => "integer", default_value => undef, is_nullable => 0, size => 4 },
   "pub_id",
   { data_type => "integer", default_value => undef, is_nullable => 0, size => 4 },
 );
+__PACKAGE__->set_primary_key("pipeprocess_pub_id");
+__PACKAGE__->add_unique_constraint("pipeprocess_pub_id_pk", ["pipeprocess_pub_id"]);
 __PACKAGE__->belongs_to(
   "pipeprocess",
   "SmallRNA::DB::Pipeprocess",
@@ -24,7 +31,7 @@ __PACKAGE__->belongs_to("pub", "SmallRNA::DB::Pub", { pub_id => "pub_id" });
 
 
 # Created by DBIx::Class::Schema::Loader v0.04005
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:jM0Hno/AYCcZiF/j+572nw
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:scosBk9o+Mr3wS5mNIP2tA
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
