1e96ffed4a0cf68eedf9f21af33ca1d68bdf464d
kmr44 <kmr44@721a6f05-5756-4a90-b3fc-8e2b6c5fda0e>
2009-10-14 15:56:57 +0000

    Added SizeDist.pm and a link to raw size distribution numbers. Added descriptions to size distribution graphs.
    git-svn-id: file:///data/subversion/smallrna/tracking/trunk@1012 721a6f05-5756-4a90-b3fc-8e2b6c5fda0e

---
+++ lib/SmallRNA/Web/Controller/Plugin/SizeDist.pm
@@ -0,0 +1,78 @@
+package SmallRNA::Web::Controller::Plugin::SizeDist;
+
+=head1 NAME
+
+SmallRNA::Web::Controller::Plugin::SizeDist - Action to get raw size
+                                              distribution values
+
+=head1 SYNOPSIS
+
+=head1 AUTHOR
+
+Kim Rutherford C<< <kmr44@cam.ac.uk> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<kmr44@cam.ac.uk>.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc SmallRNA::Web::Controller::Plugin::SizeDist
+
+=over 4
+
+=back
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Kim Rutherford, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 FUNCTIONS
+
+=cut
+
+use strict;
+use warnings;
+use Carp;
+
+use base 'Catalyst::Controller';
+
+use SmallRNA::Web::Controller::Plugin::SizeDistGraph;
+
+=head2 sizedist
+
+ Usage   : Called as a Catalyst action
+ Function: Given a pipedata_id of a first_base_summary pipedata, return a size
+           distribution as a TSV file
+ Args    : pipedata_id - the id of the pipedata containing the stats in
+                         first_base_summary format
+
+=cut
+sub sizedist : Path('/plugin/sizedist/tsv') {
+  my ($self, $c, $pipedata_id) = @_;
+
+  my $schema = $c->schema();
+  my $pipedata = $schema->find_with_type('Pipedata', 'pipedata_id', $pipedata_id);
+  my ($counts_ref, $min, $max) =
+    SmallRNA::Web::Controller::Plugin::SizeDistGraph::get_pipedata_counts($c, $pipedata);
+
+  my %counts = %$counts_ref;
+  my @lengths = ($min .. $max);
+
+  my $results = (join "\t", @lengths) . "\n";
+
+  for my $base (qw(A T C G)) {
+    my @set = map { $counts{$_}{$base} || 0 } @lengths;
+    $results .= (join "\t", @set) . "\n";
+  }
+
+  $c->res->content_type('text/plain');
+  $c->res->body($results);
+}
+
+1;
