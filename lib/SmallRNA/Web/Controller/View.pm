9294b17122859eac32f6626e4b370e3c75cacc80
kmr44 <kmr44@721a6f05-5756-4a90-b3fc-8e2b6c5fda0e>
2009-10-20 16:49:14 +0000

    Added "reports" - configurable summary tables. Started a report for samples.
    git-svn-id: file:///data/subversion/smallrna/tracking/trunk@1047 721a6f05-5756-4a90-b3fc-8e2b6c5fda0e

--- lib/SmallRNA/Web/Controller/View.pm
+++ lib/SmallRNA/Web/Controller/View.pm
@@ -127,6 +127,74 @@ sub list : Local {
 
 }
 
+sub _get_order_by_field
+{
+  my $self = shift;
+  my $c = shift;
+  my $type = shift;
+
+  my $class_info = $c->config()->{class_info}->{$type};
+
+  # default: order by id
+  my $order_by = $type . '_id';
+
+  if (defined $class_info) {
+    my @order_by_fields;
+    if (defined $class_info->{order_by}) {
+      if (ref $class_info->{order_by}) {
+        @order_by_fields = @{$class_info->{order_by}};
+      } else {
+        push @order_by_fields, $class_info->{order_by};
+      }
+    } else {
+      if (defined $class_info->{display_field}) {
+        push @order_by_fields, $class_info->{display_field};
+      }
+    }
+    if (@order_by_fields) {
+      $order_by = \@order_by_fields;
+    }
+  }
+
+  return $order_by;
+}
+
+=head2 report
+
+ Function: Display a report, configuration from the config file
+ Args    : $report_name - the report name, used to find the configuration
+
+=cut
+sub report : Local {
+  my ($self, $c, $report_name) = @_;
+
+  my $st = $c->stash;
+
+  eval {
+    my $report_conf = $c->config()->{reports}->{$report_name};
+
+    $st->{title} = $report_conf->{description};
+    $st->{template} = 'view/rstable.mhtml';
+
+    my $type = $report_conf->{object_type};
+
+    $st->{type} = $type;
+
+    my $class_name = SmallRNA::DB::class_name_of_table($type);
+    my $params = { order_by => $self->_get_order_by_field($c, $type) };
+
+    $st->{rs} = $c->schema->resultset($class_name)->search(undef, $params);
+    $st->{columns} = $report_conf->{columns};
+ 
+    $st->{page} = $c->req->param('page') || 1;
+    $st->{numrows} = $c->req->param('numrows') || 20;
+  };
+  if ($@) {
+    $c->stash->{error} = qq(Can't display report for: $report_name - $@);
+    $c->forward('/start');
+  }
+}
+
 =head2 list_collection
 
  Function: Render a collection from an object of a given type
